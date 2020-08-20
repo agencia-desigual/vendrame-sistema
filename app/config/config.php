6bbdf6a9be22bc0b10b908ec5a96e1a65e8c6821
Igor Cacerez <igor.cacerez@gmail.com>
2020-08-20 10:19:20 -0300

    Upload FrameWork

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// URL base do site.
+defined('BASE_URL') OR define('BASE_URL', 'http://localhost/git/vendrame-sistema/');
+
+// URL base do storange
+defined('BASE_STORAGE') OR define('BASE_STORAGE', 'http://localhost/git/vendrame-sistema/storage/');
+
+// Session | Caso deseje que a session seja iniciada em todas as páginas
+// Apenas mude a constante para true.
+defined("OPEN_SESSION") OR define('OPEN_SESSION', true);
+
+
